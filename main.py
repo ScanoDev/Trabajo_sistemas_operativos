0530b13dbb289c600c7e35fc50a92a530b4cd24b
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-03-21 17:12:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-
-def FirstFit(memory: list, req: int, index: int):
-    for index in range(index, len(memory)):
-        if(memory[index][1]-req >= 0):
-            oldBase = memory[index][0]
-            memory[index][0]+=req
-            memory[index][1]-=req
-            return (memory, memory[index][0], memory[index][1], oldBase, index)
+def first_fit(memory: list, req: int, index: int):
+    original_length = len(memory)
+    n = original_length
+    for offset in range(n):
+        i = (index + offset) % n
+        base, size = memory[i]
+        
+        if(size >= req):
+            new_base = base + req
+            new_size = size - req
+            
+            if(new_size == 0):
+                memory.pop(i)
+                if(i == original_length - 1):
+                    return (memory, new_base, new_size, 0)
+                else:
+                    return (memory, new_base, new_size, i)
+            else:
+                memory[i] = (new_base, new_size)
+                return (memory, new_base, new_size, i)
     return None
-
-def main():
-    memoria = [[0x04800000, 0x00a00000],
-            [0x01e00000, 0x00400000],
-            [0x02500000, 0x01400000],
-            [0x07201800, 0x01200000],
-            [0x08402c00, 0x00700000],
-            [0x06d01000, 0x00500000],
-            [0x06100c00, 0x00c00000],
-            [0x05200400, 0x00f00000]]
-    req = 0x01000000
-    index=0
-    print(FirstFit(memoria, req, index))
-    #print(0x014000000-req)
-
-if __name__ == "__main__":
-    main()
